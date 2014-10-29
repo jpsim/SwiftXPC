a36f769abf8616c425dce37de906f747546bc47c
JP Simard <jp@jpsim.com>
2014-10-29 15:58:56 -0700

    Initial Commit

---
+++ SwiftXPC/SwiftXPC.h
@@ -0,0 +1,19 @@
+//
+//  SwiftXPC.h
+//  SwiftXPC
+//
+//  Created by JP Simard on 10/29/14.
+//  Copyright (c) 2014 JP Simard. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftXPC.
+FOUNDATION_EXPORT double SwiftXPCVersionNumber;
+
+//! Project version string for SwiftXPC.
+FOUNDATION_EXPORT const unsigned char SwiftXPCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftXPC/PublicHeader.h>
+
+
