242127c3b2011a984ff2fdd944a22c5dcab41376
JP Simard <jp@jpsim.com>
2014-10-29 20:32:06 -0700

    Test arrays and dictionaries

--- SwiftXPCTests/SwiftXPCTests.swift
+++ SwiftXPCTests/SwiftXPCTests.swift
@@ -30,18 +30,18 @@ class SwiftXPCTests: XCTestCase {
 
     func testOneWayDictionary() {
         let xpcDict = xpc_dictionary_create(nil, nil, 0)
-        xpc_dictionary_set_value(xpcDict, "myArray", xpc_array_create(nil, 0))
-        xpc_dictionary_set_value(xpcDict, "myDict", xpc_dictionary_create(nil, nil, 0))
-        xpc_dictionary_set_string(xpcDict, "myString", "stringValue")
-        xpc_dictionary_set_date(xpcDict, "myDate", Int64(NSDate().timeIntervalSince1970) * 1000000000)
-        xpc_dictionary_set_data(xpcDict, "myData", nil, 0)
-        xpc_dictionary_set_uint64(xpcDict, "myUInt64", 1)
-        xpc_dictionary_set_int64(xpcDict, "myInt64", 1)
-        xpc_dictionary_set_double(xpcDict, "myDouble", 1)
-        xpc_dictionary_set_bool(xpcDict, "myBool", true)
-        xpc_dictionary_set_fd(xpcDict, "myFileHandle", 0)
+        xpc_dictionary_set_value(xpcDict, "Array", xpc_array_create(nil, 0))
+        xpc_dictionary_set_value(xpcDict, "Dictionary", xpc_dictionary_create(nil, nil, 0))
+        xpc_dictionary_set_string(xpcDict, "String", "string")
+        xpc_dictionary_set_date(xpcDict, "Date", Int64(NSDate().timeIntervalSince1970) * 1000000000)
+        xpc_dictionary_set_data(xpcDict, "Data", nil, 0)
+        xpc_dictionary_set_uint64(xpcDict, "UInt64", 1)
+        xpc_dictionary_set_int64(xpcDict, "Int64", 1)
+        xpc_dictionary_set_double(xpcDict, "Double", 1)
+        xpc_dictionary_set_bool(xpcDict, "Bool", true)
+        xpc_dictionary_set_fd(xpcDict, "FileHandle", 0)
         
-        let dict: [String:XPCRepresentable] = fromXPC(xpcDict)
+        let dict: XPCDictionary = fromXPC(xpcDict)
         XCTAssertEqual(dict.count, 10, "XPCDictionary should have the same number of items as XPC dictionary")
     }
 
@@ -73,17 +73,37 @@ class SwiftXPCTests: XCTestCase {
         testEqualityOfXPCRoundtrip(NSDate(timeIntervalSince1970: -10_000))
     }
 
-    // TODO: Fix array tests
-//    func testArrays() {
-//        testEqualityOfXPCRoundtrip([String]())
-//        testEqualityOfXPCRoundtrip(["foo"])
-//        testEqualityOfXPCRoundtrip(["foo", "bar", "baz"])
-//    }
+    func testArrays() {
+        // Empty
+        testEqualityOfXPCRoundtrip([] as XPCArray)
 
-    // TODO: Fix dictionary tests
-//    func testDictionaries() {
-//        testEqualityOfXPCRoundtrip([String: String]())
-//        testEqualityOfXPCRoundtrip(["foo": "bar"])
-//        testEqualityOfXPCRoundtrip(["foo": "bar", "theAnswerToEverything": 42])
-//    }
+        // Complete
+        // TODO: Test Array, Dictionary, FileHandle
+        testEqualityOfXPCRoundtrip([
+            "string",
+            NSDate(),
+            NSData(),
+            UInt64(0),
+            Int64(0),
+            0.0,
+            false
+            ] as XPCArray)
+    }
+
+    func testDictionaries() {
+        // Empty
+        testEqualityOfXPCRoundtrip([:] as XPCDictionary)
+
+        // Complete
+        // TODO: Test Array, Dictionary, FileHandle
+        testEqualityOfXPCRoundtrip([
+            "String": "string",
+            "Date": NSDate(),
+            "Data": NSData(),
+            "UInt64": UInt64(0),
+            "Int64": Int64(0),
+            "Double": 0.0,
+            "Bool": false
+            ] as XPCDictionary)
+    }
 }
